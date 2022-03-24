eac3661b67a4f8440690cad9ea262be41f4018a7
Levani16 <lnakashidze5@gmail.com>
2022-03-24 16:17:24 +0400

    Update window class

--- src/main/java/render/Window.java
+++ src/main/java/render/Window.java
@@ -2,81 +2,135 @@ package render;
 
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
 import org.lwjgl.opengl.GL;
-import util.Time;
-
-import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.glfw.GLFW.glfwShowWindow;
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.system.MemoryUtil.NULL;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.system.MemoryUtil;
 
 public class Window {
-    private static long windowID;
-    private static Window window = null;
+    private final String title;
     private int width, height;
-    private String title;
-    public void createWindow(int width, int height, String title){
+    private long window;
+    private boolean resize, vSync;
+
+    public Window(String title, int width, int height, boolean vSync) {
+        this.title = title;
         this.width = width;
         this.height = height;
-        this.title = title;
-        init();
-        loop();
-
+        this.vSync = vSync;
     }
 
-    public void loop(){
-        float startTime = Time.getTime();
-        float endTime = Time.getTime();
-
-        while(!glfwWindowShouldClose(windowID)){
-            glfwPollEvents();
-            glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
-            glClear(GL_COLOR_BUFFER_BIT);
-            glfwSwapBuffers(windowID);
-        }
-        endTime = Time.getTime();
-        float dt = endTime - startTime;
-        startTime = endTime;
-    }
-
-    public void init(){
+    public void init() {
         GLFWErrorCallback.createPrint(System.err).set();
-        if(!glfwInit()){
-            throw new IllegalStateException("unable to initialize GLFW");
+
+        if (!GLFW.glfwInit())
+            throw new IllegalStateException("Unable to initialize GLFW");
+
+        GLFW.glfwDefaultWindowHints();
+        GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GL11.GL_FALSE);
+        GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GL11.GL_TRUE);
+        GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MAJOR, 3);
+        GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MINOR, 2);
+        GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_PROFILE, GLFW.GLFW_OPENGL_CORE_PROFILE);
+        GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_FORWARD_COMPAT, GL11.GL_TRUE);
+
+        boolean maximised = false;
+        if (width == 0 || height == 0) {
+            width = 100;
+            height = 100;
+            GLFW.glfwWindowHint(GLFW.GLFW_MAXIMIZED, GLFW.GLFW_TRUE);
+            maximised = true;
         }
-        glfwDefaultWindowHints();
-        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
-        glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
-        windowID = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
-        if (windowID == NULL){
-            throw new IllegalStateException("failed to create GLFW window");
+
+        window = GLFW.glfwCreateWindow(width, height, title, MemoryUtil.NULL, MemoryUtil.NULL);
+        if(window == MemoryUtil.NULL)
+        throw new RuntimeException("Failed to create GLFW window");
+
+        GLFW.glfwSetFramebufferSizeCallback (window, (window, width, height) -> {
+            this.width = width;
+            this.height = height;
+            this.setResize(true);
+        });
+
+        GLFW.glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+            if(key == GLFW.GLFW_KEY_ESCAPE && action == GLFW.GLFW_RELEASE)
+                GLFW.glfwSetWindowShouldClose (window, true);
+        });
+
+        if(maximised)
+            GLFW.glfwMaximizeWindow(window);
+        else{
+            GLFWVidMode vidMode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
+            GLFW.glfwSetWindowPos (window, (vidMode.width() - width) / 2,
+                    (vidMode.height() - height) / 2);
         }
-        glfwMakeContextCurrent(windowID);
-        glfwSwapInterval(1);
-        glfwShowWindow(windowID);
+
+        GLFW.glfwMakeContextCurrent(window);
+
+        if(isvSync())
+            GLFW.glfwSwapInterval(1);
+
+        GLFW.glfwShowWindow(window);
+
         GL.createCapabilities();
+
+        GL11.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        GL11.glEnable(GL11.GL_DEPTH_TEST);
+        GL11.glEnable(GL11.GL_STENCIL_TEST);
+        GL11.glEnable(GL11.GL_CULL_FACE);
+        GL11.glCullFace(GL11.GL_BACK);
     }
 
-    public static Window get(){
-        if(Window.window == null){
-            Window.window = new Window();
-        }
-        return Window.window;
+    public void update(){
+        GLFW.glfwSwapBuffers(window);
+        GLFW.glfwPollEvents();
     }
 
-    public void updateWindow() {
-        glfwSwapBuffers(windowID);
-        glfwPollEvents();
-        glfwSwapInterval(1);
+    public void cleanup(){
+        GLFW.glfwDestroyWindow(window);
     }
 
-    public static void closeWindow() {
-        glfwDestroyWindow(windowID);
-        glfwTerminate();
+    public void setClearColour (float r, float g, float b, float a) {
+        GL11.glClearColor(r, g, b, a);
     }
 
-    public boolean shouldClose() {
-        return glfwWindowShouldClose(windowID);
+    public boolean isKeyPressed (int keycode) {
+        return GLFW.glfwGetKey(window, keycode) == GLFW.GLFW_PRESS;
+    }
+
+    public boolean shouldClose(){
+        return GLFW.glfwWindowShouldClose(window);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        GLFW.glfwSetWindowTitle(window, title);
+    }
+
+    public boolean isResize() {
+        return resize;
+    }
+
+    public boolean isvSync() {
+        return vSync;
+    }
+
+    public void setResize(boolean resize) {
+        this.resize = resize;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public long getWindow() {
+        return window;
     }
 }
