00c2d65399dfe56dee6da66ff286e05735319983
Levani16 <lnakashidze5@gmail.com>
2022-03-24 16:19:21 +0400

    Create engine launcher

---
+++ src/main/java/main/EngineLauncher.java
@@ -0,0 +1,25 @@
+package main;
+
+import render.Window;
+import util.Const;
+
+public class EngineLauncher {
+    private static Window window;
+    private static EngineCore engine;
+
+    public static void main(String[] args) {
+
+        window = new Window(Const.TITLE, 1600, 900, false);
+        engine = new EngineCore();
+
+        try{
+            engine.start();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static Window getWindow() {
+        return window;
+    }
+}
