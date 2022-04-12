d504bb3b965390bcb928dd3400cbf5b9740fd164
temopkhakadze <temopkhakadze2002@gmail.com>
2022-04-12 13:28:33 +0400

    Update import versions

--- src/main/java/MeshShader/Mesh.java
+++ src/main/java/MeshShader/Mesh.java
@@ -1,6 +1,12 @@
 package MeshShader;
 
-import static org.lwjgl.opengles.GLES20.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL30.*;
+import static org.lwjgl.opengl.GL33.*;
+
+import org.lwjgl.opengl.*;
 
 public class Mesh {
     private int vbo;
@@ -13,6 +19,7 @@ public class Mesh {
 
     public void addVertices(Vertex[] vertices) {
         this.size = vertices.length;
+
         glBindBuffer(GL_ARRAY_BUFFER, this.vbo);
         glBufferData(GL_ARRAY_BUFFER, Util.createFlippedBuffer(vertices), GL_STATIC_DRAW);
     }
