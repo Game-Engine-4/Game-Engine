213c4744400a2c7385d59f3dc445509a04e879a4
temopkhakadze <temopkhakadze2002@gmail.com>
2022-04-27 03:23:15 +0400

    Update MeshShader Package

--- src/main/java/MeshShader/Vertex.java
+++ src/main/java/MeshShader/Vertex.java
@@ -6,8 +6,16 @@ public class Vertex {
     public static final int SIZE = 3;
     private Vector3f pos;
 
+    public Vertex(){
+        pos = new Vector3f(0F, 0F, 0F);
+    }
+
     public Vertex(Vector3f pos) {
         this.pos=pos;
+    }
+
+    public Vertex(float x1, float y1, float z1){
+        pos = new Vector3f(x1, y1, z1);
     }
 
     public Vector3f getPos() {
