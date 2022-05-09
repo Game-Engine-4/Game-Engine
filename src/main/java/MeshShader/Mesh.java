5385f8707d465d5974704d2e56f60240fdb37293
NPkhakadze <nikapkhakadze00@gmail.com>
2022-05-09 16:50:57 +0400

    Add index buffer object and modify methods according to it

--- src/main/java/MeshShader/Mesh.java
+++ src/main/java/MeshShader/Mesh.java
@@ -11,17 +11,21 @@ import org.lwjgl.opengl.*;
 public class Mesh {
     private int vbo;
     private int size;
+    private int ibo;
 
     public Mesh() {
         this.vbo = glGenBuffers();
+        this.ibo=glGenBuffers();
         this.size = 0;
     }
 
-    public void addVertices(Vertex[] vertices) {
-        this.size = vertices.length;
+    public void addVertices(Vertex[] vertices, int[] indices) {
+        this.size = indices.length;
 
         glBindBuffer(GL_ARRAY_BUFFER, this.vbo);
         glBufferData(GL_ARRAY_BUFFER, Util.createFlippedBuffer(vertices), GL_STATIC_DRAW);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,ibo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, Util.createFlippedBuffer(indices), GL_STATIC_DRAW);
     }
 
     public void draw() {
@@ -30,7 +34,8 @@ public class Mesh {
         glBindBuffer(GL_ARRAY_BUFFER, vbo);
         glVertexAttribPointer(0, 3, GL_FLOAT, false, Vertex.SIZE * 4, 0);
 
-        glDrawArrays(GL_TRIANGLES, 0, size);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,ibo);
+        glDrawElements(GL_TRIANGLES,size, GL_UNSIGNED_INT,0);
 
         glDisableVertexAttribArray(0);
     }
