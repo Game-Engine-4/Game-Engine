473b4941ce143a7ece9386748f010f7b85fb8600
LukaViper <luka.karoiani7@gmail.com>
2022-05-11 19:31:18 +0400

    Updated Matrix4x4test

--- src/test/java/Math/Matrix4x4Test.java
+++ src/test/java/Math/Matrix4x4Test.java
@@ -1,22 +1,35 @@
 package Math;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
 class Matrix4x4Test {
-    Matrix4x4 matrix1 = new Matrix4x4();
-
-    @org.junit.jupiter.api.Test
-    void add() {
-
+    Matrix4x4 matrix1;
+    Matrix4x4 matrix2;
+    @BeforeEach
+    void init(){
+        matrix1 = new Matrix4x4(0,2,0,2,0,2,0,2,0,2,0,2,0,2,0,2);
+        matrix2 = new Matrix4x4(0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
+    void testSetIdentity() {
+        Matrix4x4 matrix3 = new Matrix4x4(1,0,0,0,0,1,0,0,0,0,0,1,0,0,0,01);
+        matrix1.setIdentity();
+        assertEquals(matrix3, matrix1);
+    }
+
+    @Test
     void testAdd() {
+        Matrix4x4 matrix3 = new Matrix4x4(0,3,0,3,0,3,0,3,0,3,0,3,0,3,0,3);
+        assertEquals(matrix3, Matrix4x4.add(matrix1,matrix2));
     }
 
-    @org.junit.jupiter.api.Test
-    void multiply() {
-    }
-
-    @org.junit.jupiter.api.Test
+    @Test
     void testMultiply() {
+        Matrix4x4 matrix3 = new Matrix4x4(0,4,0,4,0,4,0,4,0,4,0,4,0,4,0,4);
+        assertEquals(matrix3, Matrix4x4.multiply(matrix1, matrix2));
     }
 }
