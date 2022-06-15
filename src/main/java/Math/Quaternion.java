94a721e7020781754e847a1b8b11c109aa01e632
temopkhakadze <temopkhakadze2002@gmail.com>
2022-06-15 22:45:03 +0400

    Create JavaDoc for Quaternion class

--- src/main/java/Math/Quaternion.java
+++ src/main/java/Math/Quaternion.java
@@ -1,12 +1,21 @@
 package Math;
 
+/**
+ * Quaternions extends a rotation in three dimensions and operations on them
+ */
 public class Quaternion {
-
     private float x;
     private float y;
     private float z;
     private float w;
 
+    /**
+     * Constructor instantiates a new Quaternion object from the given list of parameters
+     * @param x - The x value of the quaternion
+     * @param y - The y value of the quaternion
+     * @param z - The z value of the quaternion
+     * @param w - The w value of the quaternion
+     */
     public Quaternion(float x, float y, float z, float w) {
         this.x = x;
         this.y = y;
@@ -14,11 +23,18 @@ public class Quaternion {
         this.w = w;
     }
 
-
+    /**
+     * This function returns the length of this vector
+     * @return The length of this vector
+     */
     public float length() {
         return (float) Math.sqrt(x * x + y * y + z * z + w * w);
     }
 
+    /**
+     * This function computes the normalized quaternion
+     * @return A normalized quaternion.
+     */
     public Quaternion normalize() {
 
         float length = length();
@@ -31,20 +47,38 @@ public class Quaternion {
         return this;
     }
 
+    /**
+     * This function sets the value of this quaternion to the normalized value of quaternion q
+     * @param q - The quaternion to be normalized
+     * @return The normalized quaternion
+     */
     public static Quaternion normalize(Quaternion q) {
         float l = q.length();
         return new Quaternion(q.x / l, q.y / l, q.z / l, q.w / l);
     }
 
+    /**
+     * This function conjugates this quaternion [-x, -y, -z, w]
+     * @return The conjugated quaternion
+     */
     public Quaternion conjugate() {
         return new Quaternion(-x, -y, -z, w);
     }
 
+    /**
+     * This function sets the value of this quaternion to the conjugate of quaternion q
+     * @param q - The source vector
+     * @return The conjugated quaternion
+     */
     public static Quaternion conjugate(Quaternion q) {
         return new Quaternion(-q.x, -q.y, -q.z, q.w);
     }
 
-
+    /**
+     * This function returns the Hamilton product of the instance by a quaternion
+     * @param r - Quaternion
+     * @return The product of this instance with r, in that order
+     */
     public Quaternion mul(Quaternion r) {
         final float a11 = r.getX();
         final float a12 = r.getY();
@@ -56,10 +90,15 @@ public class Quaternion {
         final float Y = y * a14 + w * a12 + z * a11 - x * a13;
         final float Z = z * a14 + w * a13 + x * a12 - y * a11;
 
-
         return new Quaternion(X, Y, Z, W);
     }
 
+    /**
+     * This function calculates the Hamilton product of two quaternions
+     * @param a1 - First quaternion
+     * @param a2 - Second quaternion
+     * @return The product a1 and a2, in that order
+     */
     public static Quaternion mul(Quaternion a1, Quaternion a2) {
         // Components of the first quaternion.
         final float a11 = a1.getX();
@@ -82,6 +121,11 @@ public class Quaternion {
         return new Quaternion(X, Y, Z, W);
     }
 
+    /**
+     * This function multiplies this quaternion by a parameter vector
+     * @param r - The vector to multiply this quaternion by
+     * @return The vector to multiply this quaternion by
+     */
     public Quaternion mul(Vector3f r) {
         float w_ = -x * r.getX() - y * r.getY() - z * r.getZ();
         float x_ = w * r.getX() + y * r.getZ() - z * r.getY();
@@ -91,6 +135,11 @@ public class Quaternion {
         return new Quaternion(x_, y_, z_, w_);
     }
 
+    /**
+     * Function that compares the current quaternion to the object, returns true if the object is an instance of a quaternion and values are equal
+     * @param v - The object to compare for equality
+     * @return True if this quaternion and the provided quaternion have roughly the same x, y, z and w values
+     */
     public boolean equals(Object v) {
         float epsilon = 0.0001f;
         if (v instanceof Quaternion) {
@@ -100,34 +149,66 @@ public class Quaternion {
         return false;
     }
 
+    /**
+     * Getter function, which returns the first vector value of the quaternion
+     * @return the first vector value of the Quaternion
+     */
     public float getX() {
         return x;
     }
 
+    /**
+     * Setter function, which sets the first vector value of the quaternion
+     * @param x - the desired first vector value to be set
+     */
     public void setX(float x) {
         this.x = x;
     }
 
+    /**
+     * Getter function, which returns the second vector value of the quaternion
+     * @return the second vector value of the quaternion
+     */
     public float getY() {
         return y;
     }
 
+    /**
+     * Setter function, which sets the second vector value of the quaternion
+     * @param y - the desired second vector value to be set
+     */
     public void setY(float y) {
         this.y = y;
     }
 
+    /**
+     * Getter function, which returns the second vector value of the quaternion
+     * @return the third vector value of the quaternion
+     */
     public float getZ() {
         return z;
     }
 
+    /**
+     * Setter function, which sets the third vector value of the quaternion
+     * @param z - the desired third vector value to be set
+     */
     public void setZ(float z) {
         this.z = z;
     }
 
+    /**
+     * Getter function, which gets the scalar value of the quaternion
+     * @return the scalar value
+     */
     public float getW() {
         return w;
     }
 
+    /**
+     * Setter function, which sets the scalar value of the quaternion
+     * @param w - the desired scalar value to be set
+     */
     public void setW(float w) {
         this.w = w;
     }
