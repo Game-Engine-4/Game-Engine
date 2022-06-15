a50a4d616242355808bc9bc5b4bdfbfa5d0b912c
LukaViper <luka.karoiani7@gmail.com>
2022-06-16 00:09:50 +0400

    Create JavaDocs for the Util classes

--- src/main/java/util/Utils.java
+++ src/main/java/util/Utils.java
@@ -4,7 +4,17 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+/**
+ * This class is responsible for load files with its path
+ */
 public class Utils {
+    /**
+     * This function reads a file from the specific path and returns a string of the file
+     *
+     * @param path the parameter of the path of the file
+     * @return return the string of the file
+     * @throws IOException throws exception if path is not reachable or correct
+     */
     public static String loadResource(String path) throws IOException {
         return Files.readString(Path.of(path));
     }
