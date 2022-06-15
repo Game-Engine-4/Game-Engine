44f5f1895acbcb2be365fb2b08aaef04c7ccf63d
NPkhakadze <nikapkhakadze00@gmail.com>
2022-06-15 21:17:11 +0400

    Add javadoc comments to Util class

--- src/main/java/MeshShader/Util.java
+++ src/main/java/MeshShader/Util.java
@@ -6,15 +6,33 @@ import java.nio.IntBuffer;
 
 import Math.Matrix4x4;
 
+/**
+ * Helper class for Mesh which provides float/int buffers for array of vertex and array of indices
+ */
 public class Util {
+    /**
+     * Helper method for 'createFlippedBuffer' creating float buffer according to size
+     * @param size size of array of vertices
+     * @return float buffer
+     */
     public static FloatBuffer createFloatBuffer(int size){
         return BufferUtils.createFloatBuffer(size);
     }
 
+    /**
+     * Helper method for creating int buffer
+     * @param size size of array of indices
+     * @return int buffer
+     */
     public static IntBuffer createIntBuffer(int size) {
         return BufferUtils.createIntBuffer(size);
     }
 
+    /**
+     * Method taking array of indices to put into right format
+     * @param val array of indices
+     * @return flipped int buffer
+     */
     public static IntBuffer createFlippedBuffer(int[] val) {
         IntBuffer buffer = createIntBuffer(val.length);
         buffer.put(val);
@@ -22,6 +40,11 @@ public class Util {
         return buffer;
     }
 
+    /**
+     * Method taking array of vertex to put it into right format, flipping all the data into correct positions
+     * @param vertices array of Vertex
+     * @return
+     */
     public static FloatBuffer createFlippedBuffer(Vertex[] vertices){
 
             FloatBuffer buffer = BufferUtils.createFloatBuffer(vertices.length * Vertex.SIZE);
@@ -37,6 +60,11 @@ public class Util {
             return buffer;
     }
 
+    /**
+     * Method creating flipped buffer according to given matrix
+     * @param value matrix containing data used to set uniform
+     * @return flipped float buffer of given matrix
+     */
     public static FloatBuffer createFlippedBuffer(Matrix4x4 value) {
         FloatBuffer buffer = createFloatBuffer(4 * 4);
 
