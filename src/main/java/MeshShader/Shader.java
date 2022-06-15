532d1022d5b341aea105f611dfc64bd554e7457d
NPkhakadze <nikapkhakadze00@gmail.com>
2022-06-15 22:01:07 +0400

    Add javadoc comments to Shader class

--- src/main/java/MeshShader/Shader.java
+++ src/main/java/MeshShader/Shader.java
@@ -6,10 +6,17 @@ import Math.Matrix4x4;
 import Math.Vector3f;
 import static org.lwjgl.opengl.GL20.*;
 
+/**
+ * Abstract base class for programmable shader code, Shaders take the input from the previous pipeline stage
+ * (e.g. vertex positions, colors, and rasterized pixels) and customize the output to the next stage.
+ */
 public class Shader {
     private int program;
     private HashMap<String, Integer> uniforms;
 
+    /**
+     * Class constructor that is called by any Shader implementation
+     */
     public Shader() {
         this.program = GL20.glCreateProgram();
         uniforms = new HashMap<String, Integer>();
@@ -19,10 +26,17 @@ public class Shader {
             System.exit(1);
         }
     }
+    /**
+     * Method which installs a program object as part of current rendering state
+     */
     public void bind() {
         GL20.glUseProgram(this.program);
     }
 
+    /**
+     * Method taking some string and make shader to start tracking of it
+     * @param uniform name of uniform
+     */
     public void addUniform(String uniform) {
         int uniformLocation = glGetUniformLocation(program, uniform);
 
@@ -35,6 +49,9 @@ public class Shader {
         uniforms.put(uniform, uniformLocation);
     }
 
+    /**
+     * Method which compiles a GLSL shader into binary data so that it can be used by a WebGLProgram.
+     */
     public void compileShader() {
         GL20.glLinkProgram(program);
         if(GL20.glGetProgrami(program, GL20.GL_LINK_STATUS) == 0) {
@@ -49,6 +66,11 @@ public class Shader {
         }
     }
 
+    /**
+     * Method which attaches a shader object to a program object
+     * @param text String parameter to show path of shader
+     * @param type Parameter to show shader type
+     */
     public void addProgram(String text, int type) {
         int shader = GL20.glCreateShader(type);
         if(shader == 0){
@@ -64,22 +86,43 @@ public class Shader {
         GL20.glAttachShader(program, shader);
     }
 
+    /**
+     * Method to get current program
+     * @return Integer, current rendering state
+     */
     public int getProgram() {
         return program;
     }
 
+    /**
+     * Method to change value of uniform according to name and int value
+     * @param uniformName Name of uniform
+     * @param value Integer value to set to uniform
+     */
     public void setUniformi(String uniformName, int value) {
         glUniform1i(uniforms.get(uniformName), value);
     }
-
+    /**
+     * Method to change value of uniform according to name and float value
+     * @param uniformName Name of uniform
+     * @param value Float value to set to uniform
+     */
     public void setUniformf(String uniformName, float value) {
         glUniform1f(uniforms.get(uniformName), value);
     }
-
+    /**
+     * Method to change value of uniform according to name and vector - getting coordinates from it
+     * @param uniformName Name of uniform
+     * @param value 3-element vector
+     */
     public void setUniform(String uniformName, Vector3f value) {
         glUniform3f(uniforms.get(uniformName), value.getX(), value.getY(), value.getZ());
     }
-
+    /**
+     * Method to change value of uniform according to name and matrix
+     * @param uniformName Name of uniform
+     * @param value Matrix used to get flipped buffer
+     */
     public void setUniform(String uniformName, Matrix4x4 value) {
         glUniformMatrix4fv(uniforms.get(uniformName), true, Util.createFlippedBuffer(value));
     }
