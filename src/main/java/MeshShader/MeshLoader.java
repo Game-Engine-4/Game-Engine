431d40a3f56d8b8b0f68aee248f5c83d18af6815
NPkhakadze <nikapkhakadze00@gmail.com>
2022-06-15 22:05:50 +0400

    Add javadoc comments to MeshLoader interface

--- src/main/java/MeshShader/MeshLoader.java
+++ src/main/java/MeshShader/MeshLoader.java
@@ -2,6 +2,15 @@ package MeshShader;
 
 import java.io.IOException;
 
+/**
+ * Class used to load Mesh from file which contains data
+ */
 public interface MeshLoader {
+    /**
+     * Method to load mesh from file
+     * @param filename Name of the file which should be opened and read data from it
+     * @return Mesh object derived from data of file
+     * @throws IOException Exception thrown if file couldn't be opened
+     */
     Mesh loadMesh(String filename) throws IOException;
 }
