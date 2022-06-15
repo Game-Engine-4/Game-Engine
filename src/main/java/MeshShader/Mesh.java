accc59b3307802070df688d5c07016d9f9df766a
NPkhakadze <nikapkhakadze00@gmail.com>
2022-06-15 21:00:52 +0400

    Add javadoc comments to Mesh

--- src/main/java/MeshShader/Mesh.java
+++ src/main/java/MeshShader/Mesh.java
@@ -4,17 +4,29 @@ import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL15.*;
 import static org.lwjgl.opengl.GL20.*;
 
+/**
+ * Base class for representing a 3D geometric surface
+ */
 public class Mesh {
     private int vbo;
     private int size;
     private int ibo;
 
+    /**
+     * Constructor that is called by any Mesh implementation
+     */
     public Mesh() {
         this.vbo = glGenBuffers();
         this.ibo = glGenBuffers();
         this.size = 0;
     }
 
+    /**
+     * Method which adds some data from vertices to add them at pointer 'vbo' - vertex buffer object
+     * and data from indices to 'ibo' - index buffer object
+     * @param vertices array of Vertex
+     * @param indices array of indices that specifies which indexes are actually going to be drawn
+     */
     public void addVertices(Vertex[] vertices, int[] indices) {
         this.size = indices.length;
 
@@ -24,6 +36,9 @@ public class Mesh {
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, Util.createFlippedBuffer(indices), GL_STATIC_DRAW);
     }
 
+    /**
+     * Method to draw a Mesh using gl
+     */
     public void draw() {
         glEnableVertexAttribArray(0);
 
