38ca4c293d1e7646d4a89fb359cf947296bec61e
LukaViper <luka.karoiani7@gmail.com>
2022-06-15 22:54:27 +0400

    Create JavaDocs for the input class

--- src/main/java/Inputs/Input.java
+++ src/main/java/Inputs/Input.java
@@ -3,7 +3,12 @@ package Inputs;
 import org.lwjgl.glfw.*;
 import render.Window;
 
-public class Input{
+/**
+ * This is an Input class. Input class consist of Keyboard and Mouse inputs and there are several methods.
+ * Some of which checks whether key or button is pressed,up or down. Some getters and so on...
+ */
+
+public class Input {
 
     private GLFWKeyCallback keyboard;
     private GLFWMouseButtonCallback mouseButtons;
@@ -18,8 +23,19 @@ public class Input{
     private static int keyPressed;
     private static int buttonPressed;
 
-    public Input(){
-        keyboard = new GLFWKeyCallback(){
+    public Input() {
+
+        /**
+         *This is a function to invoke the Keyboard key and press it down
+         * @param window   the window that received the event
+         * @param key      the keyboard key that was pressed or released
+         * @param scancode the platform-specific scancode of the key
+         * @param action   the key action.
+         * @param mods     bitfield describing which modifiers keys were held down
+         */
+
+        keyboard = new GLFWKeyCallback() {
+
             @Override
             public void invoke(long window, int key, int scancode, int action, int mods) {
                 keys[key] = (action == GLFW.GLFW_PRESS);
@@ -27,7 +43,16 @@ public class Input{
             }
         };
 
+        /**
+         *This is a function to invoke the Mouse button and press it down
+         * @param window the window that received the event
+         * @param button the mouse button that was pressed or released
+         * @param action the button action.
+         * @param mods   bitfield describing which modifiers keys were held down
+         */
+
         mouseButtons = new GLFWMouseButtonCallback() {
+
             @Override
             public void invoke(long window, int button, int action, int mods) {
                 buttons[button] = (action == GLFW.GLFW_PRESS);
@@ -35,7 +60,15 @@ public class Input{
             }
         };
 
+        /**
+         *This is a function to invoke the Mouse cursor and show it
+         * @param window the window that received the event
+         * @param xpos   the new cursor x-coordinate, relative to the left edge of the content area
+         * @param ypos   the new cursor y-coordinate, relative to the top edge of the content area
+         */
+
         cursorPos = new GLFWCursorPosCallback() {
+
             @Override
             public void invoke(long window, double xpos, double ypos) {
                 cursorX = xpos;
@@ -43,7 +76,15 @@ public class Input{
             }
         };
 
+        /**
+         *This is a function to invoke the Mouse Scroll button and scroll down or up
+         * @param window  the window that received the event
+         * @param xoffset the scroll offset along the x-axis
+         * @param yoffset the scroll offset along the y-axis
+         */
+
         mouseScroll = new GLFWScrollCallback() {
+
             @Override
             public void invoke(long window, double xoffset, double yoffset) {
                 scrollX += xoffset;
@@ -52,79 +93,162 @@ public class Input{
         };
     }
 
-    public static boolean isKeyDown(int key){
+    /**
+     * This is isKeyDown function, which checks whether specific key is down or not
+     *
+     * @param key the key which maybe down
+     * @return the key which is down
+     */
+
+    public static boolean isKeyDown(int key) {
         return keys[key];
     }
 
-    public static boolean isButtonDown(int button){
+    /**
+     * This is isKeyDown function, which checks whether specific key is down or not
+     *
+     * @param button the button which may be down
+     * @return the button which is down
+     */
+    public static boolean isButtonDown(int button) {
         return buttons[button];
     }
 
-    public static boolean isKeyPressed(int key){
+    /**
+     * This is isKeyPressed function, which checks whether specific key is pressed or not
+     *
+     * @param key the key which may be pressed
+     * @return the key which is pressed
+     */
+    public static boolean isKeyPressed(int key) {
         boolean check = (keyPressed == key);
-        if(check) keyPressed = -1;
+        if (check) keyPressed = -1;
         return check;
     }
 
-    public static boolean isButtonPressed(int button){
+    /**
+     * This is isButtonPressed function, which checks whether specific button is pressed or not
+     *
+     * @param button the button which may be pressed
+     * @return the button which is pressed
+     */
+    public static boolean isButtonPressed(int button) {
         boolean check = (buttonPressed == button);
-        if(check) buttonPressed = -1;
+        if (check) buttonPressed = -1;
         return check;
     }
 
-    public static boolean isKeyUp(int key){
+    /**
+     * This is isKeyUp function, which checks whether specific key is up or not
+     *
+     * @param key the key which maybe up
+     * @return the key which is up
+     */
+    public static boolean isKeyUp(int key) {
         boolean check = (key != keyPressed);
-        if(check) keyPressed = -1;
+        if (check) keyPressed = -1;
         return check;
     }
 
-    public static boolean isButtonUp(int button){
+    /**
+     * This is isButtonUp function, which checks whether specific button is up or not
+     *
+     * @param button the button which maybe up
+     * @return the v which is up
+     */
+    public static boolean isButtonUp(int button) {
         boolean check = (button != keyPressed);
-        if(check) keyPressed = -1;
+        if (check) keyPressed = -1;
         return check;
     }
 
-    public void destroy(){
+    /**
+     * This is a destroy function, which makes every button or key free if they are pressed or down
+     */
+    public void destroy() {
         keyboard.free();
         mouseButtons.free();
         cursorPos.free();
         mouseScroll.free();
     }
 
-    public void update(){
+    /**
+     * This is an update function, which updates the key or button that is pressed or down
+     */
+    public void update() {
         GLFW.glfwSetKeyCallback(Window.getWindow(), getKeyboardCallback());
         GLFW.glfwSetMouseButtonCallback(Window.getWindow(), getMouseButtonsCallback());
         GLFW.glfwSetCursorPosCallback(Window.getWindow(), getCursorPosCallback());
     }
 
+    /**
+     * This is a getter function, which return the keyboard keys
+     *
+     * @return Keyboard keys
+     */
     public GLFWKeyCallback getKeyboardCallback() {
         return keyboard;
     }
 
+    /**
+     * This is a getter function, which return the Mouse buttons
+     *
+     * @return Mouse buttons
+     */
     public GLFWMouseButtonCallback getMouseButtonsCallback() {
         return mouseButtons;
     }
 
+    /**
+     * This is a getter function, which returns the current position of cursor
+     *
+     * @return current position of cursor
+     */
     public GLFWCursorPosCallback getCursorPosCallback() {
         return cursorPos;
     }
 
+    /**
+     * This is a getter function, which returns the current position of scroll
+     *
+     * @return current position of scroll
+     */
     public GLFWScrollCallback getMouseScrollCallback() {
         return mouseScroll;
     }
 
+    /**
+     * This is a getter function, which returns the current X coordinate of cursor
+     *
+     * @return X coordinate of cursor
+     */
     public static double getCursorX() {
         return cursorX;
     }
 
+    /**
+     * This is a getter function, which returns the current Y coordinate of cursor
+     *
+     * @return Y coordinate of cursor
+     */
     public static double getCursorY() {
         return cursorY;
     }
 
+    /**
+     * This is a getter function, which returns the current X coordinate of scroll
+     *
+     * @return current X coordinate of scroll
+     */
     public static double getScrollX() {
         return scrollX;
     }
 
+    /**
+     * This is a getter function, which returns the current Y coordinate of scroll
+     *
+     * @return current Y coordinate of scroll
+     */
     public static double getScrollY() {
         return scrollY;
     }
