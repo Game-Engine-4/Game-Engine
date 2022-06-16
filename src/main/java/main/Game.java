ae96c840abe24079c760e895378a28cc318fb439
temopkhakadze <temopkhakadze2002@gmail.com>
2022-06-16 17:49:02 +0400

    Create JavaDoc for the Game Interface

--- src/main/java/main/Game.java
+++ src/main/java/main/Game.java
@@ -1,13 +1,24 @@
 package main;
 
-import java.io.IOException;
-
+/**
+ * When someone creates to create game,this interface will be used to pass
+ * init, render, adn update functions to the engine
+ */
 public interface Game {
 
+    /**
+     * Initialize an object after its creation
+     */
     void init();
 
+    /**
+     * Updates window
+     */
     void update();
 
+    /**
+     * Renders the window
+     */
     void render();
 
 }
